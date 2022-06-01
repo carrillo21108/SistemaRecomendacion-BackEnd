06e4671aca354bce9d3527a3ab0daa5c1a90c8d1
carrillo21108 <87512111+carrillo21108@users.noreply.github.com>
2022-06-01 13:12:31 -0600

    API obtener generos

--- controllers/genre.controller.js
+++ controllers/genre.controller.js
@@ -4,7 +4,7 @@ function create(req,res){
     var params = req.body;
     session
     .run("CREATE ("+params.tag+":Genre{name:'"+params.name+"',description:'"+params.description+"'})")
-    .then(function(){
+    .then(function(){ 
         res.send({message:'Genero agregado con exito a la DB.'});
     })
     .catch(function(err){
@@ -13,6 +13,29 @@ function create(req,res){
     });
 }
 
+function getGenres(req,res){
+    var resRecord = [];
+
+    session
+    .run('match (n:Genre) return n')
+    .then(function(result){
+        result.records.forEach(function(record){
+            resRecord.push(record._fields[0].properties);
+        });
+        
+        if(resRecord.length==0){
+            res.send({message:"Generos no encontrados."});
+        }else{
+            res.send(resRecord);
+        }
+    })
+    .catch(function(err){
+        console.log(err);
+        res.status(500).send({message:'Error general'});
+    });
+}
+
 module.exports = {
-    create
+    create,
+    getGenres
 }
--- routes/genre.route.js
+++ routes/genre.route.js
@@ -4,5 +4,6 @@ var genreController = require('../controllers/genre.controller');
 var api = express.Router();
 
 api.post('/createGenre',genreController.create);
+api.get('/getGenres',genreController.getGenres);
 
 module.exports = api;
