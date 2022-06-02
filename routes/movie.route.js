a14f0fdb43cb8fc29f5c134d396ecf523e7ecd5e
carrillo21108 <87512111+carrillo21108@users.noreply.github.com>
2022-06-02 01:23:50 -0600

    Cambio recomendación género

--- routes/movie.route.js
+++ routes/movie.route.js
@@ -6,6 +6,6 @@ var api = express.Router();
 api.post('/createMovie',movieController.create);
 api.post('/inGenre',movieController.genre);
 api.get('/getMovies',movieController.getMovies);
-api.get('/genreRecommendation',movieController.genreRecommendation);
+api.post('/genreRecommendation',movieController.genreRecommendation);
 
 module.exports = api;
