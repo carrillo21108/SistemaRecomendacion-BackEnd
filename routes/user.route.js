46579db0049ac83324018e9f7311ad2123ca3112
carrillo21108 <87512111+carrillo21108@users.noreply.github.com>
2022-06-01 19:39:50 -0600

    API obtener likes User

--- routes/user.route.js
+++ routes/user.route.js
@@ -10,5 +10,6 @@ api.post('/disLikeMovie',userController.dislikeMovie);
 api.post('/inLikeGenre',userController.likeGenre);
 api.post('/disLikeGenre',userController.dislikeGenre);
 api.get('/profile',userController.profile);
+api.get('/getLikesUser',userController.getLikesUser);
 
 module.exports = api;
